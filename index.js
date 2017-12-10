db38f540245513bef75845011cd75babe04f586d
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-12-10 18:24:07 +0900

    fix choo SSR

--- index.js
+++ index.js
@@ -45,14 +45,17 @@ function Choo (opts) {
   this._tree = null
 
   // state
-  var events = { events: this._events }
+  var _state = {
+    events: this._events,
+    components: {}
+  }
   if (this._hasWindow) {
     this.state = window.initialState
-      ? xtend(window.initialState, events)
-      : events
+      ? xtend(window.initialState, _state)
+      : _state
     delete window.initialState
   } else {
-    this.state = events
+    this.state = _state
   }
 
   // properties that are part of the API
@@ -210,6 +213,10 @@ Choo.prototype.toString = function (location, state) {
   assert.notEqual(typeof window, 'object', 'choo.mount: window was found. .toString() must be called in Node, use .start() or .mount() if running in the browser')
   assert.equal(typeof location, 'string', 'choo.toString: location should be type string')
   assert.equal(typeof this.state, 'object', 'choo.toString: state should be type object')
+
+  // choo-debug replaces the state object with one that's proxied. This makes
+  // sure all state in the cache is also proxied after the module is loaded.
+  this.cache.state = this.state
 
   this.state.href = location.replace(/\?.+$/, '')
   this.state.query = nanoquery(location)
